66de01a836e1b29946753a8df9d321cd6baa933b
aarongustafson <aaron@easy-designs.net>
2010-10-20 15:54:51 -0400

    prepping 1.2.7 and running tests

--- src/eCSStender.js
+++ src/eCSStender.js
@@ -2,7 +2,7 @@
 Function:      eCSStender()
 Author:        Aaron Gustafson (aaron at easy-designs dot net)
 Creation Date: 2006-12-03
-Version:       1.2.6.7
+Version:       1.2.7
 Homepage:      http://eCSStender.org
 License:       MIT License (see homepage)
 ------------------------------------------------------------------------------*/
@@ -125,7 +125,7 @@ License:       MIT License (see homepage)
   // eCSStender Object
   eCSStender = {
     name:      ECSSTENDER,
-    version:   '1.2.6.7',
+    version:   '1.2.7',
     fonts:     [],
     pages:     {},
     at:        {},
